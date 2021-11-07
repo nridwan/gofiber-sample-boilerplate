64ec035a3d64cf86dd1481a16635d24e7ce478ab
Nie Ridwan Susanto <xa18ridwan@gmail.com>
2021-11-07 17:10:54 +0700

    add CORS settings

--- features/loader.go
+++ features/loader.go
@@ -1,14 +1,23 @@
 package features
 
 import (
+	"github.com/nridwan/config/configutil"
 	"github.com/nridwan/features/auth"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 //Register module routes
 func Register(app *fiber.App) {
 	app.Use(recover.New())
+	corsOrigin := configutil.Getenv("CORS_ORIGIN", "")
+	if corsOrigin != "" {
+		app.Use(cors.New(cors.Config{
+			AllowOrigins: corsOrigin,
+			AllowHeaders: "Origin, Content-Type, Accept",
+		}))
+	}
 	auth.Register(app)
 }
